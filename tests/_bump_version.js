596bb3f658a6d799f67c8982b9d6fcde86970da7
Arshad Kazmi <arshadkazmi42@gmail.com>
2021-08-15 13:12:50 +0530

    Fix test

--- tests/_bump_version.js
+++ tests/_bump_version.js
@@ -41,7 +41,7 @@ describe('lib/bump-version.js', () => {
       new BumpVersion(FILE_PATHS.INVALID_DATA);
     } catch (err) {
       expect(err.message).to.be.equals(
-        `Unable to parse file ${FILE_PATHS.INVALID}`
+        `Unable to parse file ${FILE_PATHS.INVALID_DATA}`
       );
     }
   });
